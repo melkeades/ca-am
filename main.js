a573615c1de875cbd56e23617ecf515417d058d7
melkeades <118681335+melkeades@users.noreply.github.com>
2023-07-22 08:49:21 +0300

    map 0722

--- main.js
+++ main.js
@@ -4,18 +4,18 @@ import CSSRulePlugin from 'gsap/CSSRulePlugin'
 import ScrollTrigger from 'gsap/ScrollTrigger'
 import Lenis from '@studio-freight/lenis'
 
-const devMode = 1
+const devMode = 0
 gsap.registerPlugin(CSSRulePlugin, ScrollTrigger)
 
 const sel = (e) => document.querySelector(e)
 const selAll = (e) => document.querySelectorAll(e)
 
-const videoHero = sel('.video-hero')
-const introSec = sel('.intro-sec')
-const aboutSec = sel('.about-sec')
+const videoHero$ = sel('.video-hero')
+const introSec$ = sel('.intro-sec')
+const aboutSec$ = sel('.about-sec')
 
 if (devMode) {
-  const devRemoveList = [videoHero, introSec]
+  const devRemoveList = [videoHero$, introSec$]
   document.querySelectorAll('[data-video-urls]').forEach((el) => {
     // el.querySelector('video').remove()
     // devRemoveList.push(el)
@@ -35,11 +35,11 @@ function raf(time) {
 
 requestAnimationFrame(raf)
 
-const introCard = sel('.intro-sec__card')
+const introCard$ = sel('.intro-sec__card')
 
 ScrollTrigger.create({
   // markers: true,
-  animation: gsap.timeline().to(introCard, { transform: 'translate(0%, -80%)' }),
+  animation: gsap.timeline().to(introCard$, { transform: 'translate(0%, -80%)' }),
   trigger: '.intro-sec',
   start: 'top bottom',
   end: 'bottom top',
@@ -49,11 +49,11 @@ ScrollTrigger.create({
 
 ScrollTrigger.create({
   // markers: true,
-  animation: gsap.timeline().to('.about-sec__item', { borderRadius: '0' }, '<').to(aboutSec, { padding: '0' }, '<'),
-  trigger: aboutSec,
+  animation: gsap.timeline().to('.about-sec__item', { borderRadius: '0' }, '<').to(aboutSec$, { padding: '0' }, '<'),
+  trigger: aboutSec$,
   start: 'top top',
   end: 'bottom center',
-  pin: aboutSec,
+  pin: aboutSec$,
   // anticipatePin: 1,
   // pinType: 'fixed',
   // pinSpacing: false,
@@ -61,11 +61,11 @@ ScrollTrigger.create({
   scrub: 1,
 })
 
-const mapCards = selAll('.map-sec__card')
-const mapCardsWrap = selAll('.map-sec__card-wrap')
-const mapCardsWrapIn = selAll('.map-sec__card-wrapin')
+const mapCards$ = selAll('.map-sec__card')
+const mapCardsWrap$ = selAll('.map-sec__card-wrap')
+const mapCardsWrapIn$ = selAll('.map-sec__card-wrapin')
 
-mapCardsWrap.forEach((el) => {
+mapCardsWrap$.forEach((el) => {
   gsap.set(el, { opacity: 0, position: 'fixed', top: '55%', translateY: '-50%' })
   // el.style.opacity = 0
   // el.style.display = 'none'
@@ -73,62 +73,86 @@ mapCardsWrap.forEach((el) => {
 
 ScrollTrigger.create({
   // animation: gsap.timeline().to(mapCards[0], { opacity: 0.5 }),
-  trigger: mapCards[1],
+  trigger: mapCards$[1],
   start: 'center center',
   end: 'top top',
   // pin: mapCards[1],
   scrub: 1,
   // snap: 1,
 })
-const mapSec = sel('.map-sec')
-const map = sel('.map-sec__map')
-const mapWrap = sel('.map-sec__map-wrap')
-const mapWrapIn = sel('.map-sec__map-wrapIn')
-const mapFg = sel('.map__fg')
-const mapFgWrap = sel('.map__fg-wrap')
-const mapDots = selAll('.map__dot')
-const mapBg = sel('.map__bg-img')
-const mapBgWrap = sel('.map__bg-wrap')
-
 const cardSpeed = 0.5
+const mapDot_ = 'map__dot'
+const mapDotA_ = 'map__dot--red-dog'
+const mapDotB_ = 'map__dot--fort-knox'
+const mapDotC_ = 'map__dot--usibelli'
+const mapDotActive_ = 'map__dot--active'
+const mapDotLine_ = 'map__dot__line'
 
-ScrollTrigger.create({
-  animation: gsap
-    .timeline({ defaults: { ease: 'none', duration: 5 } })
-    // .set([...mapCards], { opacity: 0.001 }, '<')
-    .to(mapFg, { y: '-20vh' }, 0)
-    .to(mapBg, { y: '-10vh' }, 0)
-    .to(mapFg, { scale: 1.2 }, 0)
-    .to(mapBg, { scale: 1.2 * 0.84 }, 0)
-    .to(
-      {},
-      {
-        onComplete: () => gsap.timeline().to(mapCardsWrap[0], { opacity: 1, top: '50%', duration: cardSpeed }),
-        onReverseComplete: () => {
-          // gsap.killTweensOf(mapCardsWrap[0])
-          gsap.timeline().to(mapCardsWrap[0], { opacity: 0, top: '55%', duration: cardSpeed })
-        },
-        duration: 0.001,
+const mapSec$ = sel('.map-sec')
+const map$ = sel('.map-sec__map')
+const mapWrap$ = sel('.map-sec__map-wrap')
+const mapWrapIn$ = sel('.map-sec__map-wrapIn')
+const mapFg$ = sel('.map__fg')
+const mapFgWrap$ = sel('.map__fg-wrap')
+const mapDots$ = selAll('.map__dot')
+const mapBg$ = sel('.map__bg-img')
+const mapBgWrap$ = sel('.map__bg-wrap')
+
+const mapDotRemoveActiveClass = () => {
+  const activeDots = [...mapDots$].filter((el) => el.classList.contains(mapDotActive_))
+  activeDots.forEach((el) => {
+    el.classList.remove(mapDotActive_)
+  })
+}
+mapDotRemoveActiveClass()
+const mapScrollInitTl = gsap
+  .timeline({ defaults: { ease: 'none', duration: 5 } })
+  .to(mapFg$, { y: '-20vh' }, 0)
+  .to(mapBg$, { y: '-10vh' }, 0)
+  .to(mapFg$, { scale: 1.2 }, 0)
+  .to(mapBg$, { scale: 1.2 * 0.84 }, 0)
+  .to(
+    {},
+    {
+      onComplete: () => {
+        gsap.timeline().to(mapCardsWrap$[0], { opacity: 1, top: '50%', duration: cardSpeed })
+        mapDots$[0].classList.add(mapDotActive_)
       },
-      3
-    )
-    .addLabel('mapIntroDone', '>'),
-  trigger: mapSec,
+      onReverseComplete: () => {
+        // gsap.killTweensOf(mapCardsWrap[0])
+        gsap.timeline().to(mapCardsWrap$[0], { opacity: 0, top: '55%', duration: cardSpeed })
+        mapDotRemoveActiveClass()
+      },
+      duration: 0.001,
+    },
+    3.5
+  )
+  .addLabel('mapIntroDone', '>')
+
+const mapInitSt = ScrollTrigger.create({
+  animation: mapScrollInitTl,
+  trigger: mapSec$,
   start: 'top 80%',
   end: 'top top',
   scrub: 1,
-  // snap: 1,
+  snap: 1,
 })
 const mapScrollTl = gsap
   .timeline({ defaults: { ease: 'none', duration: 5 } })
   .addLabel('card-a')
-  .to(mapFgWrap, { y: '-60vh' }, 0)
-  .to(mapBgWrap, { y: -60 * 0.8 + 'vh' }, 0)
+  .to(mapFgWrap$, { y: '-60vh' }, 0)
+  .to(mapBgWrap$, { y: -60 * 0.8 + 'vh' }, 0)
   .to(
     {},
     {
-      onComplete: () => gsap.timeline().to(mapCardsWrap[0], { opacity: 0, top: '45%', duration: cardSpeed }),
-      onReverseComplete: () => gsap.timeline().to(mapCardsWrap[0], { opacity: 1, top: '50%', duration: cardSpeed }),
+      onComplete: () => {
+        gsap.timeline().to(mapCardsWrap$[0], { opacity: 0, top: '45%', duration: cardSpeed })
+        mapDotRemoveActiveClass()
+      },
+      onReverseComplete: () => {
+        gsap.timeline().to(mapCardsWrap$[0], { opacity: 1, top: '50%', duration: cardSpeed })
+        mapDots$[0].classList.add(mapDotActive_)
+      },
       duration: 0.001,
     },
     2.5 // DO NOT OVERLAP EVENT TWEENS!!! EVA
@@ -138,23 +162,34 @@ const mapScrollTl = gsap
   .to(
     {},
     {
-      onStart: () => gsap.timeline().to(mapCardsWrap[1], { opacity: 1, top: '50%', duration: cardSpeed }),
+      onComplete: () => {
+        gsap.timeline().to(mapCardsWrap$[1], { opacity: 1, top: '50%', duration: cardSpeed })
+        mapDots$[1].classList.add(mapDotActive_)
+      },
       onReverseComplete: () => {
         // gsap.killTweensOf(mapCardsWrap[1])
-        gsap.timeline().to(mapCardsWrap[1], { opacity: 0, top: '55%', duration: cardSpeed })
+        gsap.timeline().to(mapCardsWrap$[1], { opacity: 0, top: '55%', duration: cardSpeed })
+        mapDotRemoveActiveClass()
       },
       duration: 0.001,
     },
     3.5
   )
-  .set([mapFgWrap, mapBgWrap], { transformOrigin: '100% 60%' })
-  .to(mapFgWrap, { scale: 1.9 }, 5)
-  .to(mapBgWrap, { scale: 1.9 * 0.8 }, 5)
+  .set([mapFgWrap$, mapBgWrap$], { transformOrigin: '100% 60%' })
+  .to(mapFgWrap$, { scale: 1.9 }, 5)
+  .to(mapBgWrap$, { scale: 1.9 * 0.8 }, 5)
   .to(
     {},
     {
-      onComplete: () => gsap.timeline().to(mapCardsWrap[1], { opacity: 0, top: '45%', duration: cardSpeed }),
-      onReverseComplete: () => gsap.timeline().to(mapCardsWrap[1], { opacity: 1, top: '50%', duration: cardSpeed }),
+      onComplete: () => {
+        gsap.timeline().to(mapCardsWrap$[1], { opacity: 0, top: '45%', duration: cardSpeed })
+        mapDotRemoveActiveClass()
+      },
+      onReverseComplete: () => {
+        gsap.timeline().to(mapCardsWrap$[1], { opacity: 1, top: '50%', duration: cardSpeed })
+        mapDots$[1].classList.add(mapDotActive_)
+      },
+
       duration: 0.001,
     },
     7.5
@@ -163,45 +198,77 @@ const mapScrollTl = gsap
   .to(
     {},
     {
-      onStart: () => gsap.timeline().to(mapCardsWrap[2], { opacity: 1, top: '50%', duration: cardSpeed }),
+      onStart: () => {
+        gsap.timeline().to(mapCardsWrap$[2], { opacity: 1, top: '50%', duration: cardSpeed })
+        mapDots$[2].classList.add(mapDotActive_)
+      },
       onReverseComplete: () => {
         // gsap.killTweensOf(mapCardsWrap[2])
-        gsap.timeline().to(mapCardsWrap[2], { opacity: 0, top: '55%', duration: cardSpeed })
+        gsap.timeline().to(mapCardsWrap$[2], { opacity: 0, top: '55%', duration: cardSpeed })
+        mapDotRemoveActiveClass()
       },
       duration: 0.001,
     },
     8.5
   )
-  .to(mapFgWrap, { y: '-85vh' }, 10)
-  .to(mapBgWrap, { y: -85 * 0.8 + 'vh' }, 10)
+  .to(mapFgWrap$, { y: '-85vh' }, 10)
+  .to(mapBgWrap$, { y: -85 * 0.8 + 'vh' }, 10)
   .to(
     {},
     {
-      onStart: () => gsap.timeline().to(mapCardsWrap[2], { opacity: 0, top: '45%', duration: cardSpeed }),
+      onStart: () => {
+        gsap.timeline().to(mapCardsWrap$[2], { opacity: 0, top: '45%', duration: cardSpeed })
+        mapDotRemoveActiveClass()
+      },
       // onComplete: () => gsap.timeline().set(mapCardsWrap[1], { opacity: 0, top: '60%' }),
-      onReverseComplete: () => gsap.timeline().to(mapCardsWrap[2], { opacity: 1, top: '50%', duration: cardSpeed }),
+      onReverseComplete: () => {
+        gsap.timeline().to(mapCardsWrap$[2], { opacity: 1, top: '50%', duration: cardSpeed })
+        mapDots$[2].classList.add(mapDotActive_)
+      },
       duration: 0.001,
     },
     14
   )
   .addLabel('finish', 15)
 
-ScrollTrigger.create({
+const mapSt = ScrollTrigger.create({
   animation: mapScrollTl,
-  trigger: mapSec,
+  trigger: mapSec$,
   // startTrigger: '.map-sec',
   // endTrigger: mapCards[2],
   start: 'top top',
   end: 'bottom+=1000 top',
   pin: '.map-sec__map-wrap',
   scrub: 1,
-  // snap: 'labelsDirectional',
+  snap: 'labelsDirectional',
   duration: { min: 0.2, max: 1 },
   // delay: 0.0,
   // anticipatePin: true,
   // pinSpacing: true,
   // pinType: 'fixed',
 })
+
+const observer = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    // if (mutationRecord.target.className === mapDotActive_) {
+    if (mutation.target.classList.contains(mapDotActive_)) {
+      window.addEventListener('scroll', () => {
+        if (mapSt.isActive || mapInitSt.isActive) {
+          const scale1 = gsap.getProperty(mapFg$, 'scale')
+          const scale2 = gsap.getProperty(mapFgWrap$, 'scale')
+          const lineWidth = (mutation.target.getBoundingClientRect().left - mapCards$[0].getBoundingClientRect().right - 30) / (scale1 * scale2)
+          console.log(scale1, scale2, lineWidth)
+          mutation.target.querySelector('.' + mapDotLine_).style.width = lineWidth + 'px'
+        }
+      })
+    }
+  })
+})
+
+for (let mapDot of mapDots$) {
+  observer.observe(mapDot, { attributes: true, attributeFilter: ['class'] })
+}
+// console.log(mapDotActive_)
 
 // const approachImg = document.querySelector('.approach-sec__img-wrap')
 // const approachTitle = document.querySelector('.approach-sec__title-fg')
