f64226faa2b3a5d80e5115341b6619c5c8b989a6
melkeades <118681335+melkeades@users.noreply.github.com>
2023-08-12 03:38:47 +0300

    swiper autoplay 80% in viewport

--- main.js
+++ main.js
@@ -321,6 +321,37 @@ function home() {
       prevEl: '.arrow-left',
     },
   })
+  aboutSwiper.autoplay.pause()
+  const target = aboutSec$
+  function callback(entries, observer) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        aboutSwiper.autoplay.resume()
+        console.log('about swiper started')
+      } else {
+        aboutSwiper.autoplay.pause()
+        console.log('about swiper stopped')
+      }
+    })
+  }
+  function createObserver(target, callback) {
+    const options = {
+      root: null,
+      threshold: 0.8, // about swiper 80% in viewport
+    }
+    const observer = new IntersectionObserver(callback, options)
+    observer.observe(target)
+  }
+
+  createObserver(target, callback) // new IntersectionObserver((entries, observer) => {
+  //   if (entries[0].isIntersecting) {
+  //   // if (entries[0].intersectionRatio >= 0.1) {
+  //     console.log('v')
+
+  //     observer.disconnect()
+  //     aboutSwiper.autoplay.resume()
+  //   }
+  // }).observe(aboutSec$)
 
   mm.add('(min-width: 992px)', () => {
     console.log('adding sc')
